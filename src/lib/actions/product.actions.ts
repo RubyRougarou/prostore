9a73c07265dc0c3fd403c15cc645f6bb213f5328
RubyRougarou <yaghoubiarshia1381@gmail.com>
2025-08-15 14:14:47 +0330

    implemented serverless neon + debugged some codes in newly created prisma.ts file.

--- src/lib/actions/product.actions.ts
+++ src/lib/actions/product.actions.ts
@@ -1,11 +1,10 @@
 "use server";
 
-import { PrismaClient } from "../../generated/prisma";
+import { prisma } from "../../../db/prisma";
 import { LATEST_PRODUCTS_LIMIT } from "@/lib/constants";
 
 // Get latest products
 export async function getLatestProducts() {
-  const prisma = new PrismaClient();
   const data = prisma.product.findMany({
     take: LATEST_PRODUCTS_LIMIT,
     orderBy: { createdAt: "desc" },
