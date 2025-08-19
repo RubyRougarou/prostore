657c43dadc806b1f8ea25ea27b52d2279c28f872
RubyRougarou <yaghoubiarshia1381@gmail.com>
2025-08-19 16:13:50 +0330

    implemented sign in functionality.

---
+++ src/app/(auth)/sign-in/credentials-signin-form.tsx
@@ -0,0 +1,95 @@
+"use client";
+
+import Link from "next/link";
+import { useActionState } from "react";
+import { useFormStatus } from "react-dom";
+
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+import { signInWithCredentials } from "@/lib/actions/user.actions";
+
+const CredentialsSignInForm = () => {
+  const [data, action] = useActionState(signInWithCredentials, {
+    success: false,
+    message: "",
+  });
+
+  const SignInButton = () => {
+    const { pending } = useFormStatus();
+
+    return (
+      <Button
+        className={"w-full bg-ruby hover:bg-hoverRuby"}
+        variant={"default"}
+        disabled={pending}
+      >
+        {pending ? "Signing in..." : "Sign In"}
+      </Button>
+    );
+  };
+
+  return (
+    <form action={action}>
+      {data && !data.success && (
+        <div
+          className={
+            "text-destructive text-center bg-gray-400 my-1.5 mx-24 rounded-full"
+          }
+        >
+          {data.message}
+        </div>
+      )}
+      <div className="space-y-6">
+        <div>
+          <Label htmlFor={"email"}>Email</Label>
+          <Input
+            id={"email"}
+            name={"email"}
+            type={"email"}
+            autoComplete={"email"}
+            required={true}
+            className={
+              "focus-visible:ring-hoverRuby focus-visible:border-none mt-1.5"
+            }
+          />
+        </div>
+        <div>
+          <Label htmlFor={"password"}>Password</Label>
+          <Input
+            id={"password"}
+            name={"password"}
+            type={"password"}
+            autoComplete={"password"}
+            required={true}
+            className={
+              "focus-visible:ring-hoverRuby focus-visible:border-none mt-1.5"
+            }
+          />
+        </div>
+        <div>
+          {/*<Button*/}
+          {/*  className={"w-full bg-ruby hover:bg-hoverRuby"}*/}
+          {/*  variant={"default"}*/}
+          {/*>*/}
+          {/*  Sing in*/}
+          {/*</Button>*/}
+          <SignInButton />
+        </div>
+        <div className="text-sm text-center text-muted-foreground">
+          Don&apos;t have an account?{" "}
+          <Link
+            href={"/sign-up"}
+            target={"_self"}
+            className={"link hover:text-ruby hover:underline"}
+          >
+            Sign up
+          </Link>
+        </div>
+      </div>
+    </form>
+  );
+};
+
+export default CredentialsSignInForm;
---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,60 @@
+import { Metadata } from "next";
+import { redirect } from "next/navigation";
+import Link from "next/link";
+import Image from "next/image";
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+import { auth } from "@/lib/auth";
+import { APP_NAME } from "@/lib/constants";
+import CredentialsSignInForm from "@/app/(auth)/sign-in/credentials-signin-form";
+
+export const metadata: Metadata = {
+  title: "Sign in",
+};
+
+const SignInPage = async () => {
+  // const { callbackUrl } = await props.searchParams;
+
+  const session = await auth();
+
+  console.log(session);
+  if (session) {
+    return redirect("/");
+  }
+
+  return (
+    <div className={"w-full max-w-md mx-auto"}>
+      <Card>
+        <CardHeader className={"space-y-4"}>
+          <Link href={"/"} className={"flex-center"}>
+            <Image
+              src={"/images/logo.svg"}
+              alt={APP_NAME}
+              width={170}
+              height={170}
+              priority={true}
+            />
+          </Link>
+          <CardTitle className={"text-center font-bold text-2xl"}>
+            Sign in
+          </CardTitle>
+          <CardDescription className={"text-center text-md"}>
+            Sign in to your account
+          </CardDescription>
+        </CardHeader>
+        <CardContent className={"space-y-4"}>
+          <CredentialsSignInForm />
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+
+export default SignInPage;
