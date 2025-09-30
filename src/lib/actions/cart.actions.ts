103a4a302fd254650c617b970b225c67da6af6e4
RubyRougarou <yaghoubiarshia1381@gmail.com>
2025-09-30 12:27:49 +0330

    implemented calculating prices and add cart to database.

--- src/lib/actions/cart.actions.ts
+++ src/lib/actions/cart.actions.ts
@@ -1,11 +1,29 @@
 "use server";
 
 import { CartItem } from "../../../types";
-import { convertToPlainObject, formatError } from "@/lib/utils";
+import { convertToPlainObject, formatError, round2 } from "@/lib/utils";
 import { cookies } from "next/headers";
 import { auth } from "@/lib/auth";
 import { prisma } from "../../../db/prisma";
-import { cartItemsSchema } from "@/lib/validators";
+import { cartItemsSchema, insertCartSchema } from "@/lib/validators";
+import { revalidatePath } from "next/cache";
+
+// Calculate cart prices
+const calcPrice = (items: CartItem[]) => {
+  const itemsPrice = round2(
+    items.reduce((acc, item) => acc + Number(item.price) * item.qty, 0),
+  );
+  const shippingPrice = round2(itemsPrice > 100 ? 0 : 10);
+  const taxPrice = round2(0.15 * itemsPrice);
+  const totalPrice = round2(itemsPrice + shippingPrice + taxPrice);
+
+  return {
+    itemsPrice: itemsPrice.toFixed(2),
+    shippingPrice: shippingPrice.toFixed(2),
+    taxPrice: taxPrice.toFixed(2),
+    totalPrice: totalPrice.toFixed(2),
+  };
+};
 
 export async function addItemToCart(data: CartItem) {
   try {
@@ -27,11 +45,34 @@ export async function addItemToCart(data: CartItem) {
     const product = await prisma.product.findFirst({
       where: { id: item.productId },
     });
+    if (!product) {
+      throw new Error("Product not found!");
+    }
 
-    return {
-      success: true,
-      message: "Item added to cart",
-    };
+    if (!cart) {
+      // Create new cart object
+      const newCart = insertCartSchema.parse({
+        userId: userId,
+        items: [item],
+        sessionCartId: sessionCartId,
+        ...calcPrice([item]),
+      });
+
+      // Add to database
+      await prisma.cart.create({
+        data: newCart,
+      });
+
+      // Revalidate product page
+      revalidatePath(`/product/${product.slug}`);
+
+      return {
+        success: true,
+        message: "Item added to cart",
+      };
+    }
+
+    // handle quantity
   } catch (error) {
     return {
       success: false,
